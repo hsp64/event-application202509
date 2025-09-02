6f03118c68bae78aabf8ea0e454124cccf6318b9
hsp64 <pphs604@gmail.com>
2025-09-02 11:06:48 +0900

    [BE] 무한스크롤 테스트

--- backend/src/test/java/com/study/event/repository/EventRepositoryImplTest.java
+++ backend/src/test/java/com/study/event/repository/EventRepositoryImplTest.java
@@ -5,10 +5,14 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Slice;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -40,5 +44,19 @@ class EventRepositoryImplTest {
         //then
     }*/
 
+    @Test
+    @DisplayName("페이징 테스트")
+    void pagingTest() {
+        //given
+        int pageNo = 16;
+        int size = 20;
+        Pageable pageable = PageRequest.of(pageNo -1, size);
+        //when
+        Slice<Event> eventSlice = eventRepository.findEvents(pageable);
+        List<Event> content = eventSlice.getContent();
+        //then
+        System.out.println("hasNext = " + eventSlice.hasNext());
+        System.out.println("size = " + content.size());
+    }
 
 }
