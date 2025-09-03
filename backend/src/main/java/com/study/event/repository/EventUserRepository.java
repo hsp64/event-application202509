cda5d50252c564b44560e74008c2f7c5c942c61c
hsp64 <pphs604@gmail.com>
2025-09-03 09:52:49 +0900

    [BE] 이메일 인증 발송 로직

--- backend/src/main/java/com/study/event/repository/EventUserRepository.java
+++ backend/src/main/java/com/study/event/repository/EventUserRepository.java
@@ -4,4 +4,5 @@ import com.study.event.domain.entity.EventUser;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface EventUserRepository extends JpaRepository<EventUser, Long> {
+    boolean existsByEmail(String email);
 }
